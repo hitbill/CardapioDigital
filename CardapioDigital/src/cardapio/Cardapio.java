7810bb1a5e35c896621f02e6b107b717cc3fef20
Paulo <Paulo@Paulo>
2014-05-20 23:11:10 -0300

    InterfaceGrafica

---
+++ CardapioDigital/src/cardapio/Cardapio.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cardapio;
+
+/**
+ *
+ * @author Paulo
+ */
+public class Cardapio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     //Cadastros janelaCadastro = new Cadastros();
+     //janelaCadastro.setVisible(true);
+        
+    }
+    
+}
---
+++ CardapioDigital/src/cardapio/Item.java
@@ -0,0 +1,27 @@
+package cardapio;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Paulo
+ */
+public class Item {
+    private String nome;
+    
+   
+    public String GetNome(){
+        return nome;
+    }
+    
+    public void SetNome(String nome){
+        this.nome = nome;
+    }
+    
+    
+    
+}
